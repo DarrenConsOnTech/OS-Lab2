8340e2a734c31a841b7fcfaaf3cf52f8629a0117
DarrenConsOnTech <darren.constantine@ontariotechu.net>
2023-02-20 23:38:10 -0500

    Update utility.c

--- utility.c
+++ utility.c
@@ -1,3 +1,10 @@
+// Group 10 - Operating Systems Lab 2 
+// Instructor: Ghadeer Abdelkader
+// Group Members:
+// Darren Constantine - 100782701
+// Md Fardin Rahman - 100704517
+// Ram Baldeo Baldeo - 100701013
+
 #include "myshell.h"
 
 int string_tokenizer(char* str, char tokens[][MAX_BUFFER]){
