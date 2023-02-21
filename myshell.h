519d2271d45ea17815ac6bbf9d16e712cf8e73fd
DarrenConsOnTech <darren.constantine@ontariotechu.net>
2023-02-20 22:55:41 -0500

    Update myshell.h

--- myshell.h
+++ myshell.h
@@ -12,24 +12,8 @@
 #define MAX_BUFFER 1024  // max line buffer
 #define MAX_ARGS 64  // max # args
 #define SEPARATORS " \t\n"  // token sparators
-#define MAX_TOKENS 10        // the command number
-#define MAX_OPEN 10  //  open 10 stdin redirection files max and 10 stdout redirection files, myshell support several I/O redirections
-#define MAX_PATH 100 //  the maxium length of file and forder 
 
 
-
-/***********************************************************************
-announce certain data types
-***********************************************************************/
-typedef struct// redirection date structure
-{ 
-	char *filename;   // redirection file name
-	char opentype[3]; // the open method of redirection files "a"   "r"   "w" 
-	char open[3];  // the open method of redirection files  ">>"   "<"   ">" 
-} Redirect;          //
-
-extern int errno;        // system error number
-extern char **environ;   // environment array
 
 
 // Function Definitions
@@ -39,20 +23,3 @@ void clear_screen();
 void pause_shell();
 void display_dir(char* path);
 void display_environs(char env[][MAX_BUFFER]);
-
-
-// int Execute(char *buffer);//the execution of the command
-// int Command_exec(char **args, const Redirect *Inputs,const Redirect *Outputs,int *states);//
-// int Error(int errortype,char **args,const Redirect *  IOputs,const int *states, char * msg) ;//error message printout
-// int Command_strtok(char *buf,char **args,int *states,Redirect *InPuts,Redirect *OutPuts);//analyse the command
-// int Command_cd(char **args,const Redirect *Inputs, int *states);// execute command 'cd' 
-// void Command_clear(void);// execute command 'clear' 
-// int Command_dir(char **args,const Redirect *Inputs, int *states);// execute command 'dir'  
-// int Command_echo(char **args,const Redirect *Inputs,int *states);// execute command 'echo' 
-// int list_environ(void);// execute the command "environ" 
-// int show_pwd(void); // execute command 'pwd' 
-// int Command_shell(FILE *inputfile,const Redirect *Outputs,const int *states);//keep reading a line from stdin or inputfile and call "Execute()" to execute the command line.
-// void Command_delay(int n);           //delay to ensure the order of processes
-// void get_fullpath(char *fullpath,const char *shortpath);  //get the full path of a file or a directory
-// int Command_help(char **args,const Redirect *Outputs,int *states);// display the user manual;    seek  for  the key word such as  <help  dir> , output the file "readme" until meeting  '#'
-// int Command_bat(char **args,const Redirect *Inputs,const Redirect *Outputs,int *states); // execute the command "myshell" with a batchfile 
