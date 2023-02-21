b9f44c9461b17d671f1ea49ab56a02bfce59a23a
DarrenConsOnTech <darren.constantine@ontariotechu.net>
2023-02-20 20:02:35 -0500

    Update utility.c

--- utility.c
+++ utility.c
@@ -33,24 +33,10 @@ void clear_screen(){
 // pauses the shell
 void pause_shell(){
 	printf("\n\nShell is now paused. Press ENTER key to continue..\n\n");
-	char code = (char)getchar();
-	while (code != '\n'){
-		code = (char)getchar();
+	char input = (char)getchar();
+	while (input != '\n'){
+		input = (char)getchar();
 	}
-}
-
-// displays help menu
-void display_help(){
-    printf("\nSupported Internal Commands:\n\n");
-    printf("help\t\t\t Lists the help menu\n");
-	printf("pwd\t\t\t Prints the path of the working directory\n");
-	printf("cd <path>\t\t Changes the working directory to the specified path\n");
-    printf("clr\t\t\t Clears the screen\n");
-    printf("dir <directory>\t\t Lists the contents of the directory\n");
-    printf("environ\t\t\t Lists all the environment strings\n");
-    printf("echo <comment>\t\t Displays comment on the command line followed by a newline\n");
-    printf("pause\t\t\t Pauses operation of the shell until the ENTER key is hit\n");
-    printf("quit\t\t\t Closes the shell [can also use 'exit']\n\n\n");
 }
 
 // displays the directory
